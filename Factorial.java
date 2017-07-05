428068aee507cc8d3860ee4156cf5cdf532dd000
SaravananRath <saravananr95m16@gmail.com>
2017-07-05 10:48:21 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Factorial {
+
+	public static void main(String[] args) throws IOException{
+		int fact=1;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the number");
+		Integer n=Integer.parseInt(br.readLine());
+		for(int i=1;i<=n;i++){
+			fact*=i;
+		}
+		System.out.println("The factorial of "+n+" is: "+fact);
+		}
+	}
