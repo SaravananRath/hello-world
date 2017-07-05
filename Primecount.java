df84502523fa5ab26a83d7c857e5b7acfbb21fba
SaravananRath <saravananr95m16@gmail.com>
2017-07-05 14:38:25 +0530

    Update Primecount.java

--- Primecount.java
+++ Primecount.java
@@ -4,26 +4,26 @@ import java.io.InputStreamReader;
 
 
 
-public class Primecount  {
+public class Main  {
 public static void main(String args[])throws IOException{
 int rs,re;	
 int sumPrime=0;
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 System.out.println("Enter the start of range");
 rs=Integer.parseInt(br.readLine());
-
 System.out.println("Enter the end of range");
 re=Integer.parseInt(br.readLine());
 
-
+if(rs>0&&re>0){
 for(int i=rs;i<=re;i++){
 	if(checkPrime(i)){
 		sumPrime+=i;
 	}
 }
-
 System.out.println(sumPrime);
-
+}
+else
+	System.out.println("Invalid Inputs");
 }
 public static boolean checkPrime(int n){
 	if(n==1)
