5fac989e55a5a927e28664dc0848ec3932187277
SaravananRath <saravananr95m16@gmail.com>
2017-07-05 12:05:18 +0530

    Create Stringint.java

---
+++ Stringint.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Stringint  {
+public static void main(String args[])throws IOException{
+	try{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter the String");
+Integer n;
+String s="";
+String str=br.readLine();
+char a[]=str.toCharArray();
+for(int i=0;i<a.length;i++){
+if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9')
+		s=s+a[i];
+}
+
+ n=Integer.parseInt(s);
+System.out.println(n);
+	}
+catch (NumberFormatException e){
+System.out.println("No numbers present in the string");
+}
+
+}
+}
