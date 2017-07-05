26dc6b5dc85fb17d76e863fced2b014b5633b764
SaravananRath <saravananr95m16@gmail.com>
2017-07-05 12:39:43 +0530

    Update Stringint.java

--- Stringint.java
+++ Stringint.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
+
 public class Stringint  {
 public static void main(String args[])throws IOException{
 	try{
@@ -13,13 +14,13 @@ String s="";
 String str=br.readLine();
 char a[]=str.toCharArray();
 for(int i=0;i<a.length;i++){
-if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9')
+if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9'||a[i]=='-'||a[i]=='-')
 		s=s+a[i];
 }
 
  n=Integer.parseInt(s);
 System.out.println(n);
-	}
+}
 catch (NumberFormatException e){
 System.out.println("No numbers present in the string");
 }
