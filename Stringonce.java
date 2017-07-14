b2c8d4d47a1fd59743d24dd71e81ce87260d1594
SaravananRath <saravananr95m16@gmail.com>
2017-07-14 12:22:38 +0530

    Update Stringonce.java

--- Stringonce.java
+++ Stringonce.java
@@ -5,7 +5,7 @@ public class Stringonce {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the string");
-		String str=br.readLine();
+		String str=br.readLine().toLowerCase();
 		char c[]=str.toCharArray();
 		int n=c.length;
 		for(int i=0;i<n;i++){
