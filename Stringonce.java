9c7630a88aa2237919af6129967ec9d62bb9b3db
SaravananRath <saravananr95m16@gmail.com>
2017-07-14 12:18:53 +0530

    Create Stringonce.java

---
+++ Stringonce.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Stringonce {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the string");
+		String str=br.readLine();
+		char c[]=str.toCharArray();
+		int n=c.length;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				if(c[i]==c[j]&&i!=j)
+				c[j]='\0';
+			}
+		}
+		for(int i=0;i<n;i++){
+			if(c[i]!='\0')
+			System.out.print(c[i]);
+		}
